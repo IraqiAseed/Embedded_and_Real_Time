962f4ff747ce0f2e7ba8f41c10572f722f5fd522
Aseed <iraqiaseed@gmail.com>
2026-04-26 23:30:47 +0300

    add assignment 10 doubleLinkedlist

---
+++ assignment_10/src/doubleLinkedList.c
@@ -0,0 +1,305 @@
+#include "../include/doubleLinkedList.h"
+#include<stdlib.h>
+typedef struct Node Node;
+
+struct Node
+{
+    void *m_data;
+    Node *m_next;
+    Node *m_prev;
+};
+
+struct List
+{
+    Node m_head;
+    Node m_tail;
+};
+
+List *ListCreate(void)
+{
+    List *list = (List *)malloc(sizeof(List));
+
+    if (list == NULL)
+    {
+        return NULL;
+    }
+
+    list->m_head.m_data = NULL;
+    list->m_head.m_next = &list->m_tail;
+    list->m_head.m_prev = NULL;
+
+    list->m_tail.m_data = NULL;
+    list->m_tail.m_prev = &list->m_head;
+    list->m_tail.m_next = NULL;
+
+    return list;
+}
+
+void ListDestroy(List **_pList, void (*_elementDestroy)(void *_item))
+{
+    if (_pList == NULL || *_pList == NULL)
+    {
+        return;
+    }
+
+    Node *current = (*_pList)->m_head.m_next;
+
+    while (current != NULL && current != &(*_pList)->m_tail)
+    {
+        Node *next = current->m_next;
+
+        if (_elementDestroy != NULL)
+        {
+            _elementDestroy(current->m_data);
+        }
+        free(current);
+
+        current = next;
+    }
+
+    free(*_pList);
+    *_pList = NULL;
+}
+// position must not be NULL and must not be the head
+// item must not be NULL
+static Node *InsertBefore(Node *position, void *item)
+{
+    if (position == NULL || item == NULL)
+    {
+        return NULL;
+    }
+    Node *node = (Node *)malloc(sizeof(Node));
+
+    if (node == NULL)
+    {
+        return NULL;
+    }
+    node->m_data = item;
+
+    node->m_prev = position->m_prev;
+    node->m_next = position;
+    position->m_prev->m_next = node;
+    position->m_prev = node;
+
+    return node;
+}
+
+ListItr ListPushHead(List *_list, void *_item)
+{
+    if (_list == NULL || _item == NULL)
+    {
+        return NULL;
+    }
+
+    // first can be the tail node if the list is empty
+    Node *first = _list->m_head.m_next;
+
+    return (ListItr)InsertBefore(first, _item);
+}
+
+ListItr ListPushTail(List *_list, void *_item)
+{
+    if (_list == NULL || _item == NULL)
+    {
+        return NULL;
+    }
+
+    return (ListItr)InsertBefore(&_list->m_tail, _item);
+}
+
+static void *RemoveNode(Node *node)
+{
+    if (node == NULL || node->m_prev == NULL || node->m_next == NULL)
+    {
+        return NULL;
+    }
+
+    node->m_prev->m_next = node->m_next;
+    node->m_next->m_prev = node->m_prev;
+
+    void* data = node->m_data;
+    free(node);
+    return data;
+}
+
+void *ListPopHead(List *_list)
+{
+    if (_list == NULL || (_list->m_head.m_next == &_list->m_tail))
+    {
+        return NULL;
+    }
+
+    return RemoveNode(_list->m_head.m_next);
+}
+
+void *ListPopTail(List *_list)
+{
+    if (_list == NULL || (_list->m_tail.m_prev == &_list->m_head))
+    {
+        return NULL;
+    }
+
+    return RemoveNode(_list->m_tail.m_prev);
+}
+
+ListItr ListItrBegin(const List *_list)
+{
+    if (_list == NULL)
+    {
+        return NULL;
+    }
+
+    return (ListItr)_list->m_head.m_next;
+}
+
+ListItr ListItrEnd(const List *_list)
+{
+    if (_list == NULL)
+    {
+        return NULL;
+    }
+
+    return (ListItr)&_list->m_tail;
+}
+
+ListItr ListItrNext(ListItr _itr)
+{
+    // tail->m_next is null, so return tail
+    if (_itr == NULL || ((Node *)_itr)->m_next == NULL)
+    {
+        return _itr;
+    }
+
+    return (ListItr)(((Node *)_itr)->m_next);
+}
+
+ListItr ListItrPrev(ListItr _itr)
+{
+    Node *node = (Node *)_itr;
+
+    if (node == NULL || node->m_prev == NULL)
+    {
+        return _itr;
+    }
+
+    // prev of head is null, so return head. if here node->prev != NULL
+    if (node->m_prev->m_prev == NULL)
+    {
+        return _itr;
+    }
+
+    return (ListItr)node->m_prev;
+}
+
+void *ListItrGet(ListItr _itr)
+{
+    // if tail return null by contract,  tail m_data already null as design
+    if (_itr == NULL || ((Node *)_itr)->m_next == NULL)
+    {
+        return NULL;
+    }
+
+    return ((Node *)_itr)->m_data;
+}
+
+void *ListItrSet(ListItr _itr, void *_element)
+{
+    if (_itr == NULL || _element == NULL || ((Node *)_itr)->m_next == NULL ||
+        ((Node *)_itr)->m_prev == NULL)
+    {
+        return NULL;
+    }
+
+    void *oldData = ((Node *)_itr)->m_data;
+    ((Node *)_itr)->m_data = _element;
+
+    return oldData;
+}
+
+ListItr ListItrInsertBefore(ListItr _itr, void *_element)
+{
+    if (_itr == NULL || _element == NULL ||
+        ((Node *)_itr)->m_prev == NULL)
+    {
+        return NULL;
+    }
+    return (ListItr)InsertBefore((Node *)_itr, _element);
+}
+
+
+void* ListItrRemove(ListItr _itr)
+{
+    if( _itr == NULL ||
+        ((Node*) _itr)->m_next == NULL ||
+        ((Node*) _itr)->m_prev == NULL )
+    {
+        return NULL;
+    }
+
+    return RemoveNode((Node *)_itr);
+}
+
+size_t ListSize(const List* _list)
+{
+    size_t size = 0;
+
+    if(_list == NULL)
+    {
+        return size;
+    }
+    
+    for(ListItr itr = ListItrBegin(_list); 
+        itr != ListItrEnd(_list);
+        itr = ListItrNext(itr))
+    {
+        ++size;
+    }
+
+    return size;
+}
+
+size_t ListIsEmpty(List* _list)
+{
+    if (_list == NULL)
+    {
+        return 1;
+    }
+
+    return (_list->m_head.m_next == &_list->m_tail) ? 1 : 0;
+}
+
+ListItr ListItrForEach(ListItr _begin, ListItr _end, ListActionFunction _action, void* _context)
+{
+    if (_begin == NULL || _end == NULL || _action == NULL)
+    {
+        return NULL;
+    }
+
+    ListItr current = _begin;
+
+    while (current != _end)
+    {
+        void *data = ListItrGet(current);
+
+        if(data == NULL)
+        {
+            break; //range validation
+        }
+        
+        if (_action(data, _context) == 0)
+        {
+            break;
+        }
+
+        current = ListItrNext(current);
+    }
+
+    return current;
+}
+
+
+
+
+
+
+
---
+++ assignment_10/src/main.c
@@ -0,0 +1,156 @@
+#include "./../include/doubleLinkedList.h"
+#include <stdio.h>
+#include <assert.h>
+
+int Action(void* data, void* context)
+{
+    *(int*)data += *(int*)context;
+    return 1;
+}
+
+int main()
+{
+    printf("Hello, World!\n");
+
+    // Test 1 — Create + Empty
+
+    List *list = ListCreate();
+    assert(ListIsEmpty(list) == 1);
+    assert(ListSize(list) == 0);
+
+    printf("Test 1 - Create + Empty Success\n");
+
+    // Test 2 Push head
+    int a = 1, b = 2, c = 3;
+
+    ListPushHead(list, &a);
+    ListPushHead(list, &b);
+    ListPushHead(list, &c);
+
+    ListItr itr = ListItrBegin(list);
+
+    // first is c = 3
+    assert(*(int *)ListItrGet(itr) == 3);
+
+    // second b = 2
+    itr = ListItrNext(itr);
+    assert(*(int *)ListItrGet(itr) == 2);
+
+    // third a = 1
+    itr = ListItrNext(itr);
+    assert(*(int *)ListItrGet(itr) == 1);
+
+    printf("Test 2 - Push Head Success\n");
+
+    // list  [b] 3 2 1 [e]
+
+    // Test 3 Push Tail
+    int d = 1, e = 2, f = 3;
+    ListPushTail(list, &d);
+    ListPushTail(list, &e);
+    ListPushTail(list, &f);
+
+    // list should be [b] 3 2 1 1 2 3 [e]
+    itr = ListItrNext(itr);
+    assert(*(int *)ListItrGet(itr) == 1);
+
+    itr = ListItrNext(itr);
+    assert(*(int *)ListItrGet(itr) == 2);
+
+    itr = ListItrNext(itr);
+    assert(*(int *)ListItrGet(itr) == 3);
+
+    // list end
+    itr = ListItrNext(itr);
+    assert(itr == ListItrEnd(list));
+    printf("Test 3 - Push Tail Success\n");
+
+    // Test 4 — Iteration
+    printf("Test 4 - list iteration - print list\n");
+    for (itr = ListItrBegin(list);
+         itr != ListItrEnd(list);
+         itr = ListItrNext(itr))
+    {
+        printf("%d ", *(int *)ListItrGet(itr));
+    }
+
+    printf("\nTest 4 - list iteration Success\n");
+
+    // Test 5 — PopHead
+    int *val = (int *)ListPopHead(list);
+    assert(*val == 3);
+    printf("Test 5 - Pop Head Success\n");
+
+    // Test 6 — PopTail
+    ListPopTail(list);
+    val = (int *)ListPopTail(list);
+    assert(*val == 2);
+    printf("Test 6 - Pop Tail Success (twice)\n");
+
+    
+    // Test InsertBefore 
+    // insert before second 1 in list
+    // cuurent list is [b] [2 1 1] [e]
+    printf("Test 7 - cuurent list is [b] [2 1 1] [e] insertBefore second 1\n");
+    itr = ListItrBegin(list);
+    int count = 0;
+    while (itr != ListItrEnd(list))
+    {
+        int val = *(int *)ListItrGet(itr);
+
+        if (val == 1)
+        {
+            count++;
+            if (count == 2)
+            {
+                break;
+            }
+        }
+
+        itr = ListItrNext(itr);
+    }
+
+    int g = 99;
+    ListItrInsertBefore(itr,&g);
+
+    //print list
+    for (ListItr itr = ListItrBegin(list);
+         itr != ListItrEnd(list);
+         itr = ListItrNext(itr))
+    {
+        printf("%d ", *(int *)ListItrGet(itr));
+    }
+    printf("\n");
+    printf("Test 7 - insertBefore Success\n");
+
+   
+    //Test 8 set 100 instead of 99
+    int h = 100;
+    ListItrSet(ListItrPrev(itr),&h);
+    
+    for (ListItr itr = ListItrBegin(list);
+         itr != ListItrEnd(list);
+         itr = ListItrNext(itr))
+    {
+        printf("%d ", *(int *)ListItrGet(itr));
+    }
+    printf("\n");
+    printf("Test 8 - Set Success\n");
+
+    
+    //Test 9 — ForEach
+    int someInteger = 4;
+    ListItrForEach(ListItrBegin(list), ListItrEnd(list), Action, &someInteger);
+    
+   for (ListItr itr = ListItrBegin(list);
+         itr != ListItrEnd(list);
+         itr = ListItrNext(itr))
+    {
+        printf("%d ", *(int *)ListItrGet(itr));
+    }
+    printf("\n");
+
+    printf("Test 9 - ForEach Success\n");
+
+    return 0;
+}
